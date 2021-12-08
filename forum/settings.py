9f968d5135f9008dcdd8ce0521822d0fad6778ec
april <ziqian0619@gmail.com>
2021-12-08 11:28:05 +0800

    master

--- forum/settings.py
+++ forum/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'forum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['pagetmpl'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
